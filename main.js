bafe9bf037fe65a804cd1b3a3824136a97fd67b9
Chinmay <chinmaytechnicals@gmail.com>
2021-01-20 16:58:33 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,8 @@
+const toggle = () =>{
+    if (document.getElementById('navbar').style.width==="200px"){
+        document.getElementById('navbar').style.width = "0";
+    }
+    else{
+        document.getElementById("navbar").style.width = "200px";
+    }
+}
